d9164c9bb54048648696ae17e1dcbe68b33e951b
DevStranger <majachlipala@student.agh.edu.pl>
2024-07-02 19:07:50 +0200

    settings update

--- High-level/wentylator/wentylator/Models/credentials.cs
+++ High-level/wentylator/wentylator/Models/credentials.cs
@@ -1,26 +1,9 @@
-﻿using System.IO;
-using System.Text.Json;
-
-namespace CoolFan.Models
+﻿namespace CoolFan.Models
 {
-    public static class credentials
+    public class credentials
     {
-        static credentials()
-        {
-            var credentialsPath = "credentials.json";
-            var json = File.ReadAllText(credentialsPath);
-            var data = JsonSerializer.Deserialize<CredentialsData>(json);
-            Username = data.Username;
-            Password = data.Password;
-        }
-
-        public static string Username { get; private set; }
-        public static string Password { get; set; }
-
-        private class CredentialsData
-        {
-            public string Username { get; set; }
-            public string Password { get; set; }
-        }
+        public const string Username = "admin";
+        public const string Password = "haslo123#";
     }
 }
+
