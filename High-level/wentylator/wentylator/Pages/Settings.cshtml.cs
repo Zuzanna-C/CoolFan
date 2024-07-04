ee759926c71b866e36583e922dc25ebbaf795621
DevStranger <majachlipala@student.agh.edu.pl>
2024-07-04 08:14:35 +0200

    settings

--- High-level/wentylator/wentylator/Pages/Settings.cshtml.cs
+++ High-level/wentylator/wentylator/Pages/Settings.cshtml.cs
@@ -1,15 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.AspNetCore.Authorization;
 
 namespace wentylator.Pages
 {
-    [Authorize]
     public class SettingsModel : PageModel
     {
-        public void OnGet()
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly SignInManager<IdentityUser> _signInManager;
+
+        public SettingsModel(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
         {
+            _userManager = userManager;
+            _signInManager = signInManager;
         }
+
     }
 }
-
-
